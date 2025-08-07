759fda9eda8e775ac396e1612011c92d014704d6
Poorvika N P <poorvikanp245@gmail.com>
2025-08-07 20:04:01 +0530

    Add solution for Second Largest - 2025-08-07

---
+++ GeeksforGeeks_DSA_Solve/Second_largest.java
@@ -0,0 +1,24 @@
+
+package GeeksforGeeks_DSA_Solve;
+import  java.util.Arrays;
+public class Second_largest {
+    public static int getSecondLargest(int[] arr) {
+    int n=arr.length;
+        if(arr.length<2){
+            return -1;
+        }
+        Arrays.sort(arr);
+        int l=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]!=l){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {12, 35, 1, 10, 34, 1};
+        System.out.println(getSecondLargest(arr));
+        
+    }
+}
